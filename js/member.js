548fc16a67e53bbc7c4d3c6004f0a3b16ac1f5f6
chenyu <123456>
2022-08-15 16:58:05 +0800

    init

--- js/member.js
+++ js/member.js
@@ -2,12 +2,12 @@ var member = [
   {
     "phone": "14",
     "name": "陈宇",
-    "location":"SG/SF/PF"
+    "location":"SG/SF/PF/C"
   },
   {
     "phone": "30",
     "name": "曾飞",
-    "location":"PG/SG/SF"
+    "location":"PG/SG/SF/PF"
   },
   {
     "phone": "18",
@@ -16,11 +16,11 @@ var member = [
   },{
     "phone": "23",
     "name": "方飞",
-    "location":"SF/PF"
+    "location":"SF/PF/C"
   },{
     "phone": "35",
     "name": "沈海明",
-    "location":"PF/C"
+    "location":"SF/PF/C"
   },
   {
     "phone": "10",
@@ -33,26 +33,49 @@ var member = [
   },{
     "phone": "15",
     "name": "吴蛟",
-    "location":"SG/SF"
+    "location":"SG/SF/PF/C"
   },{
     "phone": "9",
     "name": "姚磊",
-    "location":"SG/SF"
+    "location":"SG/SF/PF"
   },{
     "phone": "1",
     "name": "孟庆鑫",
-    "location":"SG/SF"
+    "location":"SG/SF/PF/C"
   },{
     "phone": "91",
     "name": "吴永洋",
-    "location":"PF/C"
+    "location":"SF/PF/C"
   },{
     "phone": "16",
     "name": "陈磊",
-    "location":"SF/PF"
-  },{
+    "location":"SF/PF/C"
+  }
+  ,{
     "phone": "36",
     "name": "张志宇",
     "location":"PG/SG"
-  },
+  }
+  ,{
+    "phone": "3",
+    "name": "王军",
+    "location":"SG/PG"
+  },{
+    "phone": "22",
+    "name": "王曦铭",
+    "location":"PG/SG"
+  },{
+    "phone": "8",
+    "name": "周星",
+    "location":"SG/SF/PF/C"
+  },{
+    "phone": "17",
+    "name": "孙磊",
+    "location":"SG/SF/PF/C"
+  }
+  // ,{
+  //   "phone": "19",
+  //   "name": "尤天宇",
+  //   "location":"PG/SF/PF"
+  // }
 ]
