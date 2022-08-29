3c362e92860b3574ed6d8f29448979d0ea9993ae
chenyu <123456>
2022-08-29 17:51:06 +0800

    抽奖展示

--- js/member.js
+++ js/member.js
@@ -12,7 +12,7 @@ var member = [
   {
     "phone": "18",
     "name": "施政",
-    "location":"SG"
+    "location":"SG/SF"
   },{
     "phone": "23",
     "name": "方飞",
@@ -25,33 +25,45 @@ var member = [
   {
     "phone": "10",
     "name": "罗威",
-    "location":"SG/PG"
-  },{
+    "location":"SG/PG/SF"
+  },
+  {
     "phone": "21",
     "name": "盛俊",
     "location":"PG/SG/SF"
-  },{
+  },
+  {
     "phone": "15",
     "name": "吴蛟",
     "location":"SG/SF/PF/C"
-  },{
-    "phone": "9",
-    "name": "姚磊",
-    "location":"SG/SF/PF"
-  },{
+  },
+  {
+    "phone": "7",
+    "name": "周锋",
+    "location":"PG/SG"
+  },
+  // {
+  //   "phone": "9",
+  //   "name": "姚磊",
+  //   "location":"SG/SF/PF"
+  // },
+  {
     "phone": "1",
     "name": "孟庆鑫",
     "location":"SG/SF/PF/C"
-  },{
+  },
+  {
     "phone": "91",
     "name": "吴永洋",
     "location":"SF/PF/C"
-  },{
+  },
+  {
     "phone": "16",
     "name": "陈磊",
     "location":"SF/PF/C"
   }
-  ,{
+  ,
+  {
     "phone": "36",
     "name": "张志宇",
     "location":"PG/SG"
@@ -67,7 +79,7 @@ var member = [
   },{
     "phone": "8",
     "name": "周星",
-    "location":"SG/SF/PF/C"
+    "location":"SG/SF/PF"
   },{
     "phone": "17",
     "name": "孙磊",
